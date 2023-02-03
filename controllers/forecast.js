2e82ea231d6afc006000594e93a32d954a9c064c
Abhishek Singh <202051006@iiitvadodara.ac.in>
2023-02-04 03:25:03 +0530

    fix error

--- controllers/forecast.js
+++ controllers/forecast.js
@@ -1,13 +1,14 @@
 import axios from "axios";
+import { API_KEY } from "../util/secret.js";
 export const Forecast = async (req, res) => {
   const { location, days } = req.body;
   axios
     .get(
-      `https://api.weatherapi.com/v1/forecast.json?key=508c065f49d9489c94d202827230302&q=${location}&days=${days}&aqi=no&alerts=yes`
+      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=${days}&aqi=no&alerts=yes`
     )
     .then((resp) => {
       const { current, forecast } = resp.data;
-      res.send(resp.data);
+      res.send(current);
       //console.log(resp.data);
     });
 };
--- util/secret.js
+++ util/secret.js
@@ -5,3 +5,4 @@ dotenv.config();
 export const PORT = parseInt(process.env.PORT) || 8000;
 export const MONGO_URI = process.env.MONGO_URI;
 export const SECRET_KEY = process.env.SECRET_KEY;
+export const API_KEY = process.env.API_KEY;
